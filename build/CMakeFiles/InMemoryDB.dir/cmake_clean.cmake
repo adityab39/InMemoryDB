1ce0136b3beaf48516d94099081d60930880d5d7
adityab39 <adityabahulikar39@gmail.com>
2025-11-04 12:57:43 -0500

    Added build folder

---
+++ build/CMakeFiles/InMemoryDB.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InMemoryDB.dir/main.cpp.o"
+  "CMakeFiles/InMemoryDB.dir/main.cpp.o.d"
+  "CMakeFiles/InMemoryDB.dir/store.cpp.o"
+  "CMakeFiles/InMemoryDB.dir/store.cpp.o.d"
+  "InMemoryDB"
+  "InMemoryDB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InMemoryDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/InMemoryDB.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for InMemoryDB.
+# This may be replaced when dependencies are built.
