04a5c637eff93ffebd6d36841810f5ac2a402a4c
adityab39 <adityabahulikar39@gmail.com>
2025-11-04 09:57:10 -0500

    Added all functionalities in store.cpp

--- store.cpp
+++ store.cpp
@@ -8,3 +8,60 @@ void Store::set(const std::string& key, const std::string& value) {
     std::cout << "OK\n";
 }
 
+std::string Store::get(const std::string& key) {
+    std::lock_guard<std::mutex> lock(mutex);
+    auto it = data.find(key);
+    if (it == data.end()) return "";
+    auto exp = expiry.find(key);
+    if (exp != expiry.end() && std::chrono::system_clock::now() > exp->second) {
+        data.erase(key);
+        expiry.erase(key);
+        return "";
+    }
+    return it->second;
+}
+
+
+void Store::del(const std::string& key) {
+    std::lock_guard<std::mutex> lock(mutex);
+    data.erase(key);
+    expiry.erase(key);
+    std::cout << "OK\n";
+}
+
+
+void Store::setTTL(const std::string& key, int seconds) {
+    std::lock_guard<std::mutex> lock(mutex);
+    if (data.find(key) != data.end()) {
+        expiry[key] = std::chrono::system_clock::now() + std::chrono::seconds(seconds);
+        std::cout << "OK\n";
+    } else {
+        std::cout << "Key does not exist.\n";
+    }
+}
+
+
+void Store::removeExpiredKeys() {
+    std::lock_guard<std::mutex> lock(mutex);
+    auto now = std::chrono::system_clock::now();
+    for (auto it = expiry.begin(); it != expiry.end(); ) {
+        if (now > it->second) {
+            data.erase(it->first);
+            it = expiry.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
+
+
+void Store::listKeys() {
+    std::lock_guard<std::mutex> lock(mutex);
+    for (const auto& pair : data) {
+        std::cout << pair.first << "\n";
+    }
+}
+
+std::mutex& Store::getMutex() {
+    return mutex;
+}
